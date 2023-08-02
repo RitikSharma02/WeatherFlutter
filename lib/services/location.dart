5f95ecaf2d5ab43c10527cd86cdd56be04db42ec
RITIK PANDAT <pandatritik47@gmail.com>
2023-08-02 23:21:25 +0530

    First_day-update

---
+++ lib/services/location.dart
@@ -0,0 +1,34 @@
+import 'package:geolocator/geolocator.dart';
+
+Future<Position> getCurrentLocationCheckingPermissions() async {
+  bool serviceEnabled;
+  LocationPermission locationPermission;
+
+  // Test if location services are enabled.
+  serviceEnabled = await Geolocator.isLocationServiceEnabled();
+  if (!serviceEnabled) {
+    // Location services are not enabled don't continue
+    // accessing the position and request users of the
+    // App to enable the location services.
+    throw Exception('Location services are disabled. Please activate them.');
+  } else {
+    locationPermission = await Geolocator.checkPermission();
+    if (LocationPermission.unableToDetermine == locationPermission) {
+      throw Exception(
+          'Unable to determine if location permissions are enabled.');
+    } else if (LocationPermission.denied == locationPermission ||
+        LocationPermission.deniedForever == locationPermission) {
+      throw Exception(
+          'Location permissions are denied: ' + locationPermission.toString());
+    }
+
+    if (LocationPermission.whileInUse == locationPermission ||
+        LocationPermission.always == locationPermission) {
+      Position position = await Geolocator.getCurrentPosition(
+        desiredAccuracy: LocationAccuracy.lowest,
+      );
+      return position;
+    }
+  }
+  throw Exception('Location permissions are not granted.');
+}
